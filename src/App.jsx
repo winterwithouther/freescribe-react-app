e35a81f3688b4d96f5ff7e2094fb1d29125b17c4
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-07-31 20:47:31 -0400

    created Header and Homepage components and moved contents in ther

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import React from 'react'
+import HomePage from './components/HomePage'
+import Header from "./components/Header"
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      
-    </>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header/>
+        <HomePage/>
+      </section>
+      <footer></footer>
+    </div>
   )
 }
-
-export default App
